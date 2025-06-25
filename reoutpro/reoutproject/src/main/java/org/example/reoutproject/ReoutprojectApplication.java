2b0fe93020ee6a045253c5203a567d4b82f65e54
YoungJae1118 <apricity1118@gmail.com>
2025-06-25 15:43:51 +0900

    Initial commit

---
+++ reoutpro/reoutproject/src/main/java/org/example/reoutproject/ReoutprojectApplication.java
@@ -0,0 +1,13 @@
+package org.example.reoutproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReoutprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReoutprojectApplication.class, args);
+    }
+
+}
